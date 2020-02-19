6c003fc6b66ce5edaabca2ac18ce68567d184c02
Alex Golding <alexgolding@Alexs-MacBook-Pro.local>
2020-02-18 20:52:06 -0500

    frontend commit 3

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,10 +4,10 @@ switch (window.location.hostname) {
     // this is the local host name of your react app
     case 'localhost' || '127.0.0.1':
         // this is the local host name of your API
-        APIURL = 'http://localhost:4000/';
+        APIURL = 'http://localhost:4000';
         break;
     // this is the deployed react application
-    case 'ag-happygrillmore-log.herokuapp.com/':
+    case 'ag-happygrillmore-log.herokuapp.com':
         // this is the full url of your deployed API
         APIURL = 'https://happygrillmore-server.herokuapp.com'
 }
